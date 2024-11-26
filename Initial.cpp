a85a8d1fba7097cc8c76c20ebb018a34c71a8ee3
DyneStein <dyssart@gmail.com>
2024-11-26 17:24:03 +0500

    changed name

---
+++ Initial.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+        cout<<"Hello , I am Dyen Asif.";
+        cout<<"Welcome to my Github.";
+        cout<<"Here you will find my practice codes and my project codes.....updates.";
+
+        cout<<"Consider giving me star if you understand my code. XD";
+
+
+    return 0;
+}
