9094f44e5e43c1afb822189a694d23056b123bc6
DyneStein <dyssart@gmail.com>
2024-12-18 00:30:26 +0500

    constant diagonal prgram

---
+++ Arrays_Playground/DiagonalConstant.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void check_diagonals(int array[],int R);
+int main()
+{
+
+
+
+
+    
+
+    return 0;
+}
+
+void check_diagonals(int array[],int R)
+{
+
+
+
+}
