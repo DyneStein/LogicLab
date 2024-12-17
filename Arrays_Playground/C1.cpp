e0ddf82ac4977a43abd2d35739f07b880e7728fc
DyneStein <dyssart@gmail.com>
2024-12-17 18:26:28 +0500

    backtracking

--- Arrays_Playground/C1.cpp
+++ Arrays_Playground/C1.cpp
@@ -21,57 +21,68 @@ for(int i = 0; i<size;i++)
     }
 }
 
-int k = 1;
-cout<<matrix[0][0];
 int j = 0;
 int i = 0;
+int k = 0;
+bool restrictor = false;
+bool go = true;
+
 
 int data[2][size*size];
 data[0][0] = 0;
 data[1][0] = 0;
 
-while( k<(size*size - 1) )
+while(k<(size*size))
 {
 
-    
-
-
-if(j+1<size &&  j+1 != data[1][k-2])
-{ j++;
-cout<<matrix[i][j];
-
-data[1][k] = j;
-data[0][k] = i;
-}
-else if(i+1<size && i+1 != data[0][k-2])
+if(j+1<size && j+1!=data[1][k] && go)
 {
-    i++;
-    cout<<matrix[i][j];
-
-data[1][k] = j;
-data[0][k] = i;
-}
-else if(j-1>=0 && j-1 != data[1][k-2])
-{
-    j--;
-    cout<<matrix[i][j];
-data[1][k] = j;
-data[0][k] = i;
-}
-else if(i-1>=0 && i-1 != data[0][k-2]) 
-{
-    i--;
-    cout<<matrix[i][j];
-    data[1][k] = j;
     data[0][k] = i;
+    data[1][k] = j;
+    cout<<matrix[i][j];
+    j++;
+}
+else if(i+1<size && i+1!=data[0][k] && go)
+{
+    data[0][k] = i;
+    data[1][k] = j;
+    cout<<matrix[i][j];
+    i++;
+}
+else if(j-1>=0 && j-1!=data[1][k] && go)
+{
+    data[0][k] = i;
+    data[1][k] = j;
+    cout<<matrix[i][j];
+    j--;
+}
+else if(i-1>=0 && i-1!=data[0][k] && go)
+{
+    data[0][k] = i;
+    data[1][k] = j;
+    cout<<matrix[i][j];
+    i--;
+    restrictor = true;
+    go = false;
 }
 
-    k++;
+
+
+if(restrictor)
+{
+    if(j!=0 && i!=0)
+    {
+        cout<<matrix[i][j];
+        j--;
+    }
+    else{
+        return 0;
+    }
 }
 
 
-
-
+k++;
+}
 
 
 
